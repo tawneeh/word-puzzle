61ad0ad151dd9b7bc70261d799b2ca5ffe59cd4f
Tawnee Harris <tawneeh@icloud.com>
2020-10-27 12:03:00 -0700

    add functional for loop in scripts.js
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
 
-
-
-  }
+  const vowels = ["a","e","i","o","u"]
+  let string = "Believe you can and you're halfway there. Theodore Roosevelt"
+  for (let i = 0; i < string.length; i +=1) {
+    if (vowels.includes(string[i]) === true) {
+      string = string.replace(string[i], "-")
+      console.log(string)}
+  };
 });
+
+
+
+
+
+
+
+/*  const vowels = ["a","e","i","o","u"]
+
+  let string = "Believe you can and you're halfway there. Theodore Roosevelt";
+  for (let i = 0; i < string.length; i +=1) {
+    if vowels.includes("[i]") === true {
+      string.replace([i], "-")
+    }
+    console.log(string)
+      });
+    
+  }; */
